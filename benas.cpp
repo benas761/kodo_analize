1356e29ce762c0da7a88c2f297876878aa9ebfe8
Benas Žitkevičius <benasi89@gmail.com>
2022-11-07 21:17:32 +0200

    Reformated benas.cpp

--- benas.cpp
+++ benas.cpp
@@ -1,84 +1,86 @@
-void naujasFailas(list <mokinys> m, int psk, int k){
-    string name;
-    char vard[6] = {'V', 'a', 'r', 'd', 'a', 's'}, pav[7] = {'P', 'a', 'v', 'a', 'r', 'd', 'e'};
-    int fx=1000*pow(10, k);
-    name = to_string(fx) + ".txt";
-    ofstream fr1(name.c_str());
+void NewFile(list <mokinys> student, int homework_count, int student_count_thousands){
+    string file_name;
+    char name_string[6] = {'V', 'a', 'r', 'd', 'a', 's'};
+    char last_name_str[7] = {'P', 'a', 'v', 'a', 'r', 'd', 'e'};
+    int student_count=1000*pow(10, student_count_thousands);
+    file_name = to_string(homework_count) + ".txt";
+    ofstream fr1(file_name.c_str());
     fr1 << left << setw(20) << "Pavarde" << setw(20) << "Vardas";
-    for(int i=0; i<psk; i++)
-    fr1 << "ND" << setw(8) << 1+i;
+    for(int i=0; i<homework_count; i++)
+        fr1 << "ND" << setw(8) << 1+i;
     fr1 << setw(10) << "Egz." << endl;
-    m.push_back(mokinys());
-    list <mokinys> :: iterator it = m.begin();
-    for(int i=0; i<fx; i++){
+    students.push_back(mokinys());
+    list <mokinys> :: iterator student_iterator = students.begin();
+    for(int i=0; i<student_count; i++){
         for(int j=0; j<6; j++){
-            it->n = psk;
-            it->pav.push_back(pav[j]);
-            it->vard.push_back(vard[j]);
+            student_iterator->n = homework_count;
+            student_iterator->pav.push_back(last_name[j]);
+            student_iterator->vard.push_back(vard[j]);
         }
-        it->pav.push_back(pav[6]);
-        m.push_back(mokinys());
-        it++;
+        student_iterator->pav.push_back(last_name[6]);
+        students.push_back(mokinys());
+        student_iterator++;
     }
-    it--;
-    it = m.begin();
-    for(int j=0; j<fx; j++){                      //pazymiu generavimas
-        for(int i=0; i<it->n; i++){
+    student_iterator--;
+    student_iterator = students.begin();
+    for(int j=0; j<student_count; j++){                      //pazymiu generavimas
+        for(int i=0; i<student_iterator->n; i++){
             int z=rand()%10+1;
-            it->nd.push_back(z);
+            student_iterator->nd.push_back(z);
         }
-        it->e=rand()%10+1;
-        it++;
+        student_iterator->e=rand()%10+1;
+        student_iterator++;
     }
-    it = m.begin();
-    for(int i=0; i<fx; i++){                    //Rasymas i faila
-        fr1 << it->pav << setw(13) << 1+i << it->vard << setw(14) << i+1;
-        for(list <double> :: iterator it2 = it->nd.begin(); it2!=it->nd.end(); it2++)
-            fr1 << setw(10) << *it2;
-        fr1 << setw(10) << it->e << endl;
-        it++;
+    student_iterator = students.begin();
+    for(int i=0; i<student_count; i++){                    //Rasymas i faila
+        fr1 << it->pav << setw(13) << 1+i << student_iterator->vard << setw(14) << i+1;
+        for(list <double> :: iterator homework_iterator = student_iterator->nd.begin(); 
+            homework_iterator!=it->nd.end(); homework_iterator++)
+            fr1 << setw(10) << *homework_iterator;
+        fr1 << setw(10) << student_iterator->e << endl;
+        student_iterator++;
     }
     fr1.close();
 }
 
-void nuskaitymas(list <mokinys> &m, int &C, int k){
-    m.push_back(mokinys());
-    list <mokinys> :: iterator it = m.begin();
-    C=0;
-    char c; int words = -3; bool space = true; int ND;
-    int fx=1000*pow(10, k);
-    string name = to_string(fx) + ".txt", vard, pav;
-    ifstream fd(name.c_str());
-    while(fd.get(c) and c!='\n'){           //skaiciuoja kiek nd
-        if(std::isspace(c)){
-            space=true;
+void Reading(list <mokinys> &students, int &student_count, int student_count_thousands){
+    students.push_back(mokinys());
+    list <mokinys> :: iterator student_iterator = students.begin();
+    student_count=0;
+    char character; int words = -3; bool is_space = true; int homework_grade;
+    int student_count=1000*pow(10, student_count_thousands);
+    string file_name = to_string(student_count) + ".txt", name, last_name;
+    ifstream fd(file_name.c_str());
+    while(fd.get(character) and character!='\n'){           //skaiciuoja kiek nd
+        if(std::isspace(character)){
+            is_space=true;
         }
         else if(space){
             words++;
-            space=false;
+            is_space=false;
         }
     }
-    for(int j=0; j<fx; j++){                       //nuskaito duomenis is failo
-        m.push_back(mokinys());
-        it->n=words;
-        fd >> pav;
-        char P[pav.length()];
-        strcpy(P, pav.c_str());
-        for(int i=0; i<pav.length(); i++)
-            it->pav.push_back(P[i]);
-        fd >> vard;
-        char H[vard.length()];
-        strcpy(H, vard.c_str());
+    for(int j=0; j<student_count; j++){                       //nuskaito duomenis is failo
+        students.push_back(mokinys());
+        student_iterator->n=words;
+        fd >> last_name;
+        char last_name_chars[last_name.length()];
+        strcpy(last_name_chars, last_name.c_str());
+        for(int i=0; i<last_name.length(); i++)
+            student_iterator->pav.push_back(last_name_chars[i]);
+        fd >> name;
+        char name_chars[name.length()];
+        strcpy(name_chars, name.c_str());
         for(int i=0; i<vard.length(); i++)
-            it->vard.push_back(H[i]);
+            student_iterator->vard.push_back(name_chars[i]);
         for(int i=0; i<words; i++){
-            fd >> ND;
-            it->nd.push_back(ND);
+            fd >> homework_grade;
+            student_iterator->nd.push_back(homework_grade);
         }
         fd >> it->e;
-        C++;
-        it++;
+        student_count++;
+        student_iterator++;
     }
-    m.pop_back();
-    C--;
+    students.pop_back();
+    student_count--;
 }
--- rolandas.cpp
+++ rolandas.cpp
@@ -1,69 +1,69 @@
-void CreateInput(int n, int m) { // pakeista i sablona
-	std::random_device rd;
-	std::mt19937 mt0(rd());
-	std::uniform_int_distribution<int> grades(1, 10);
-	std::ofstream fr("generated.txt"); // one change only pls
-	fr << setw(33) << std::left << "Vardas" << setw(30) << std::left << "Pavarde";
-	for(int j = 0; j < m; j++) fr << setw(3) << std::right << j+1 << "nd";
-	fr << setw(5) << "egz." << endl;
-	for(int i=1; i<=n; i++) {
-		fr << "Vardas" << setw(27) << std::left << i;
-		fr << "Pavarde" << setw(26) << std::left << i;
-		for(int j = 0; j < m+1; j++) fr << setw(5) << grades(mt0);
-		fr << endl;
+void CreateInput(int student_number, int homework_number) { // funkcija skaičių generavimui
+	std::random_device random_device;
+	std::mt19937 mt0(random_device());
+	std::uniform_int_distribution<int> grades(1, 10);  //generuojami skaičiai nuo 1 iki 10
+	std::ofstream file_read("generated.txt");
+	file_read << setw(33) << std::left << "Vardas" << setw(30) << std::left << "Pavarde";
+	for(int j = 0; j < homework_number; j++) file_read << setw(3) << std::right << j+1 << "nd";
+	file_read << setw(5) << "egz." << endl;
+	for(int i=1; i<=student_number; i++) {
+		file_read << "Vardas" << setw(27) << std::left << i;
+		file_read << "Pavarde" << setw(26) << std::left << i;
+		for(int j = 0; j < homework_number+1; j++) file_read << setw(5) << grades(mt0);
+		file_read << endl;
 	}
-	fr.close();
+	file_read.close();
 }
 
-void FileInput(vector<stud> &x, string input) {
-	string str; stud temp;
-	std::ifstream fd(input);
-	getline(fd, str); // Praleidzia pirma linija
-	while(getline(fd, str)) {
-		x.push_back(temp);
-		std::istringstream ss(str);
-		ss >> x.back().name >> x.back().lname;
-		int in;
-	    while(ss >> in) if(in <= 10 || in > 0) x.back().nd.push_back(in);
-	    if(x.back().nd.size() > 1) {
-	    	x.back().exam = x.back().nd.back();
-	    	x.back().nd.pop_back();
+void FileInput(vector<student> &x, string input) {
+	string _string; stud temporary;
+	std::ifstream file(input);
+	getline(file, _string); // Praleidzia pirma linija
+	while(getline(file, _string)) {
+		x.push_back(temporary);
+		std::istringstream string_stream(_string);
+		string_stream >> x.back().name >> x.back().last_name;
+		int integer;
+	    while(string_stream >> integer) if(integer <= 10 || integer > 0) x.back().homework.push_back(integer);
+	    if(x.back().homework.size() > 1) {
+	    	x.back().exam = x.back().homework.back();
+	    	x.back().homework.pop_back();
 		}
-		else if(x.back().nd.size() == 0) {
-			x.back().exam = x.back().nd.back();
-			x.back().nd.back() = 0;
+		else if(x.back().homework.size() == 0) {
+			x.back().exam = x.back().homework.back();
+			x.back().homework.back() = 0;
 		}
 		else {
 			x.back().exam = 0;
-			x.back().nd.back() = 0;
+			x.back().homework.back() = 0;
 		}
 	}
-	fd.close();
+	file.close();
 }
 
 void CmdInput(vector<stud> &x) {
 	using std::cin;
-	string str; stud temp;
-    getline(cin, str);  // Ignoruoja pasilikusi \n (getline, nes ignore tarpus priskaiciuoja)
+	string _string; stud temporary;
+    getline(cin, _string);  // Ignoruoja pasilikusi \n (getline, nes ignore tarpus priskaiciuoja)
 	cout << "Iveskite mokiniu vardus, pavardes ir pazymius (egzamino pazymys paskutinis, vienas mokinys - viena eilute)\nNorint baigti iveskite tuscia eilute.\n";
-	while(getline(cin, str)) {
-		if(str == "") break;
-		x.push_back(temp);
-		std::istringstream ss(str);
-		ss >> x.back().name >> x.back().lname;
-		int in;
-	    while(ss >> in) if(in <= 10 || in > 0) x.back().nd.push_back(in);
-	    if(x.back().nd.size() > 1) {
-	    	x.back().exam = x.back().nd.back();
-	    	x.back().nd.pop_back();
+	while(getline(cin, _string)) {
+		if(_string == "") break;
+		x.push_back(temporary);
+		std::istringstream string_stream(_string);
+		string_stream >> x.back().name >> x.back().lastname;
+		int integer;
+	    while(string_stream >> in) if(integer <= 10 || integer > 0) x.back().homework.push_back(integer);
+	    if(x.back().homework.size() > 1) {
+	    	x.back().exam = x.back().homework.back();
+	    	x.back().homework.pop_back();
 		}
-		else if(x.back().nd.size() == 1) {
-			x.back().exam = x.back().nd.back();
-			x.back().nd.back() = 0;
+		else if(x.back().homework.size() == 1) {
+			x.back().exam = x.back().homework.back();
+			x.back().homework.back() = 0;
 		}
 		else {
 			x.back().exam = 0;
-			x.back().nd.push_back(0);
+			x.back().homework.push_back(0);
 		}
 	}
 }
